947e04ed90d00b8364a1b1a755cda52e41eafe00
User <hungryas@gmail.com>
2022-05-29 19:34:51 +0500

    Added integration tests.

--- src/test/java/Week08/service/UsersServiceIntegrationTest.java
+++ src/test/java/Week08/service/UsersServiceIntegrationTest.java
@@ -3,12 +3,15 @@ package Week08.service;
 import Week08.config.PostgreSQLContainerInitializer;
 import Week08.model.User;
 import Week08.repository.UsersRepository;
+import Week08.service.exception.RequiredFieldMissedException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.util.UUID;
 
 @SpringBootTest
 @ContextConfiguration(initializers = {PostgreSQLContainerInitializer.class})
@@ -43,5 +46,77 @@ class UsersServiceIntegrationTest {
         user = usersService.create(user);
         Assertions.assertEquals(user, usersRepository.findById(user.getId())
                 .orElseThrow(() -> new RuntimeException("User not found")));
+    }
+
+    @Test
+    void shouldNotCreateUserWithoutFirstName() {
+        User user = new User(
+                null,
+                null,
+                TEST_USER_LAST_NAME,
+                TEST_USER_MIDDLE_NAME,
+                TEST_USER_PHONE
+        );
+        Assertions.assertThrows(RequiredFieldMissedException.class, () -> usersService.create(user));
+    }
+
+    @Test
+    void shouldNotCreateUserWithoutLastName() {
+        User user = new User(
+                null,
+                TEST_USER_FIRST_NAME,
+                null,
+                TEST_USER_MIDDLE_NAME,
+                TEST_USER_PHONE
+        );
+        Assertions.assertThrows(RequiredFieldMissedException.class, () -> usersService.create(user));
+    }
+
+    @Test
+    void shouldCreateUserWithoutOptionalFields() {
+        User user = new User(
+                null,
+                TEST_USER_FIRST_NAME,
+                TEST_USER_LAST_NAME,
+                null,
+                null
+        );
+        Assertions.assertDoesNotThrow(() -> usersService.create(user));
+    }
+
+    @Test
+    void shouldNotEqualWithUser() {
+        User user = new User(
+                null,
+                TEST_USER_FIRST_NAME,
+                TEST_USER_LAST_NAME,
+                TEST_USER_MIDDLE_NAME,
+                TEST_USER_PHONE
+        );
+        Assertions.assertNotEquals(TEST_USER, user);
+    }
+
+    @Test
+    void shouldNotEqualWithUserHashCode() {
+        User user = new User(
+                null,
+                TEST_USER_FIRST_NAME,
+                TEST_USER_LAST_NAME,
+                TEST_USER_MIDDLE_NAME,
+                TEST_USER_PHONE
+        );
+        Assertions.assertEquals(TEST_USER.hashCode(), user.hashCode());
+    }
+
+    @Test
+    void shouldNotEqualWithNewUser() {
+        User user = new User(
+                UUID.randomUUID().toString(),
+                TEST_USER_FIRST_NAME,
+                TEST_USER_LAST_NAME,
+                TEST_USER_MIDDLE_NAME,
+                TEST_USER_PHONE
+        );
+        Assertions.assertNotEquals(TEST_USER, user);
     }
 }
